398320966e80fb0b4dcab15aae6a2cac40b343b6
YBO <yitzhak851@gmail.com>
2024-03-17 19:05:03 +0200

    adding few classes

--- Tile.java
+++ Tile.java
@@ -1,5 +1,31 @@
-    public class Tile {
-        // field
+    public abstract class Tile {
+        // member field
         int tileCoordinate;
+
+        // constructor
+        public Tile (int tileCoordinate){
+            this.tileCoordinate = tileCoordinate;
+        }
+        public abstract boolean isTileOccupied() {
+            
+        }
+        public abstract Piece getPiece ();
+        public static final class EmptyTile extends Tile {
+            EmptyTile(int coordinate){
+                super(coordinate);
+            }
+            @Override
+            public boolean isTileOccupied(){
+                return false;
+            }
+            @Override
+            public Piece getPiece(){
+                return null;
+            }
+        }
+        public static final class OccupiedTile extends Tile {
+            Piece pieceOnTile;
+            
+        }
 
     }
